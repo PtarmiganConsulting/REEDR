d622905353960e6608808a558b5850d11d5a8437
Christian Douglass <christian@ptarmiganconsulting.com>
2022-10-19 17:26:08 -0700

    added error handling in new format to nearly all Excel user inputs

--- Scripts/datavalidation.py
+++ Scripts/datavalidation.py
@@ -1,6 +1,11 @@
 import os
 
 def validate(input_name, input_value, input_type, value_low, value_high, valid_list, file_path=None):
+        
+    #First check if string is empty
+    if str(input_value) == "nan":
+        print("\n*** ERROR: " + str(input_name) + " field cannot be blank. Please specify a valid " + str(input_name) + ". ***\n")
+        raise Exception
     
     # check if input is within valid list
     if input_type == "list":
@@ -12,11 +17,23 @@ def validate(input_name, input_value, input_type, value_low, value_high, valid_l
             print("This data field must be set to one of the following allowed values: \n\n" + valid_list_string + "\n")
             raise Exception
 
+    # check if input is a file that exists
+    if input_type == "file":
+        #... then check if file exists
+        filename = input_value + ".txt"
+        if os.path.exists(os.path.join(file_path, filename)) == True:
+            return input_value
+        else:
+            print("\n*** ERROR: Could not find " + str(input_name) + " called: " + str(input_value) + "\" ***")
+            print("Make sure the file " + str(filename) + " exists at the path: " + str(file_path) + "\n")
+            raise Exception
+    
     # check if input is number between a low and high value
     if input_type == "num_between":
         if input_value >= value_low and input_value <= value_high:
             return input_value
         else:
+            print("\n*** ERROR: " + str(input_name) + " must be a number between " + str(value_low) + " and " + str(value_high) + " ***\n")
             raise Exception
 
     # check if input is number between a low and high value
@@ -24,21 +41,18 @@ def validate(input_name, input_value, input_type, value_low, value_high, valid_l
         if input_value != 0 and input_value / input_value == 1:
             return input_value
         else:
+            print("\n*** ERROR: " + str(input_name) + " must be numeric and greater than zero. ***\n")
             raise Exception
 
     # check if input is number between a low and high value
     if input_type == "any_num":
-        if int(input_value) == 0:
+        if type(input_value) == str:
+            print("\n*** ERROR: " + str(input_name) + " must be numeric. ***\n")
+            raise Exception
+        elif input_value == 0:
             return input_value
         elif input_value / input_value == 1:
             return input_value
         else:
-            raise Exception
-
-    # check if input is a file that exists
-    if input_type == "file":
-        filename = input_value + ".txt"
-        if os.path.exists(os.path.join(file_path, filename)) == True:
-            return input_value
-        else:
+            print("\n*** ERROR: " + str(input_name) + " must be numeric. ***\n")
             raise Exception
