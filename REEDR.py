efad7c122cc547aedf487b448888f8ba5e4eaa3a
Christian Douglass <christian@ptarmiganconsulting.com>
2024-02-12 10:45:40 -0800

    updated license and README files.

--- REEDR.py
+++ REEDR.py
@@ -1,6 +1,6 @@
 #*******************************************************************************************************************************************************************
 
-#Copyright (C) 2023 Ptarmigan Consulting LLC
+#Copyright (C) 2024 Ptarmigan Consulting LLC
 
 #This file is part of REEDR.
 
@@ -15,7 +15,7 @@
 #*******************************************************************************************************************************************************************
 
 print("***********************************************************************************************************************")
-print("Copyright (C) 2023 Ptarmigan Consulting LLC\n")
+print("Copyright (C) 2024 Ptarmigan Consulting LLC\n")
 print("This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n")
 print("This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.\n")
 print("You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>")
